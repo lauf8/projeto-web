dab9c57fdf30f0df711c2e27cbcf2f4c6575b213
lauf8 <lucasafonsodesouza@gmail.com>
2024-03-20 14:14:38 -0300

    refactory: criar uma organização de pastas

--- site2/javascript.js
+++
@@ -1,9 +0,0 @@
-//tela de login
-function longar() {
-    window.location = "filmes.html"
-}
-
-// botão voltar das páginas dos filmes para a página filme
-function votar() {
-    window.location = "filmes.html"
-}
---
+++ site2/js/javascript.js
@@ -0,0 +1,9 @@
+//tela de login
+function longar() {
+    window.location = "../index.html"
+}
+
+// botão voltar das páginas dos filmes para a página filme
+function votar() {
+    window.location = "../index.html"
+}
